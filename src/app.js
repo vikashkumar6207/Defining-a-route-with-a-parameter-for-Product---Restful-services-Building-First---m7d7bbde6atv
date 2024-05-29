3615a189fdb585cc0ed875406858ada48a7cac1c
vikashkumar6207 <136398175+vikashkumar6207@users.noreply.github.com>
2024-05-29 20:49:36 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/product/:productId', (req, res) => {
+  //Write a code here for endpoint /product/:productId and also print parameter in json form
+});
+
+module.exports = app;
